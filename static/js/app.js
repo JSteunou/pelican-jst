4d23fa063850be7f135f11c93dd8680cc61bfa33
Jérôme Steunou <g_rom_sempai@hotmail.com>
2013-09-09 15:11:33 +0200

    add comimoc front add style for comment headers

---
+++ static/js/app.js
@@ -0,0 +1,80 @@
+
+
+(function(comimoc, angular) {
+    
+    comimoc.factory('Comments', ['$resource', 'COMIMOC_CONFIG', function($resource, COMIMOC_CONFIG) {
+        // expose `Comments` resource service
+        
+        // transform on query GET
+        // because the API never return array
+        // but object containing array (security reason)
+        var transformResponse = function(data) {
+            var jsonData = angular.fromJson(data);
+            return jsonData.comments;
+        };
+        
+        return $resource(COMIMOC_CONFIG.RESOURCES_LOCATION + "/comments",
+                         {},
+                         {'query':  {method:'GET',  isArray: true, transformResponse: transformResponse}});
+        
+    }]);
+    
+    comimoc.controller('CommentCtrl', ['$scope', 'Comments', 'COMIMOC_CONFIG', function($scope, Comments, COMIMOC_CONFIG) {
+        // handle getting and posting new comments
+        
+        
+        // -- private --
+        
+        var getPage = function() {
+            var page = window.location.pathname;
+            if(COMIMOC_CONFIG.ARGS) {
+                page += window.location.search;
+            }
+            if(COMIMOC_CONFIG.HASH) {
+                page += window.location.hash;
+            }
+            
+            return page;
+        };
+        
+        var getWebsite = function() {
+            return COMIMOC_CONFIG.WEBSITE || '';
+        };
+        
+        var normalize = function(comment) {
+            comment.when = new Date(comment.when);
+            return comment;
+        };
+        
+        
+        // -- public --
+        
+        $scope.comments = Comments.query({website: getWebsite(), page: getPage()});
+        $scope.comments.$promise.then(function(comments) {
+            comments.forEach(function(comment) {
+               comment = normalize(comment);
+            });
+        });
+        $scope.comment = new Comments();
+        
+        $scope.submit = function(valid) {
+            if(valid) {
+                $scope.comment.website = getWebsite();
+                $scope.comment.page = getPage();
+                
+                $scope.comment.$save().then(
+                    function(comment) {
+                        $scope.comment = new Comments();
+                        $scope.comments.push(normalize(comment));
+                    },
+                    function(resp) {
+                        $scope.error = resp;
+                    });
+            }
+        };
+            
+    }]);
+
+
+})(angular.module('comimoc'), angular);
+
